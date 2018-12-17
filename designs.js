4bcbc0b76e1da38d253854a9b2ded10a7e8268d3
Ghaida-Alsalmi <ghaida.sid@gmail.com>
2018-12-17 18:51:44 +0300

    Initial commit

---
+++ designs.js
@@ -0,0 +1,35 @@
+const form = document.getElementById('sizePicker');
+let canvas = document.getElementById('pixelCanvas');
+
+function makeGrid() {
+    //Define user input (Height and Width)
+    const h = document.getElementById('inputHeight').value;
+    const w = document.getElementById('inputWidth').value;
+
+    console.log(h, w);
+    //create the table based on user input.
+    for (let row = 0; row < h; row++) {
+        let tr = document.createElement('tr');
+        canvas.appendChild(tr);
+        for (let col = 0; col < w; col++) {
+            let td = document.createElement('td');
+            canvas.lastChild.appendChild(td);
+        }
+    }
+
+    // Each cell have an event listener that sets the background color
+    let cells = document.querySelectorAll('td');
+    cells.forEach(function(cell) {
+        cell.addEventListener('click', function() {
+            this.style.backgroundColor = document.querySelector('#colorPicker').value;
+        });
+    });
+}
+
+//add event listener When size is submitted and call makeGrid()
+form.addEventListener('submit', function(e) {
+    //Clear the canvas when user re-submit
+    canvas.innerHTML = '';
+    e.preventDefault();
+    makeGrid();
+});
